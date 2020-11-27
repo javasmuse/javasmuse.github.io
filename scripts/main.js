c50978cd165ce4badc4bd57c4b8681a493c2b558
Candace Lewis <candace@Mac-mini.local>
2020-11-27 14:41:42 -0500

    api check

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+let response = fetch('https://api.windy.com/api/webcams/v2/list/category=beach/country=IT/orderby=popularity,desc/limit=20?Xt36Ggwve9BzZlKpksj5un84AoujvTyI').then((response) => {
+    return response.json();
+}).then((data) => {
+    console.log(data);
+    console.log(data.name);
+
+}).catch((err) => {
+    console.log(error);
+
+});
